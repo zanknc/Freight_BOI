82594614dd07c3d2b0cd6cde8cc516a86e1b9273
zanknc <Zank_zazn@hotmail.com>
2022-06-09 17:04:06 +0700

    Zank 09/06/2022 #1

--- Import_Freight_BOI/Import_Freight_BOI/Controllers/Manage_FreightController.cs
+++ Import_Freight_BOI/Import_Freight_BOI/Controllers/Manage_FreightController.cs
@@ -30,13 +30,10 @@ namespace Import_Freight_BOI.Controllers
         }
         public IActionResult Import_Freight()
         {
+            
             String Strsql = "";
             var ObjRun = new mgrSQLConnect(_configuration);
-            DataTable dt_Country = new DataTable();
-            DataTable dt_Delivery = new DataTable();
-            DataTable dt_Forwarder = new DataTable();
-            DataTable dt_Purpose = new DataTable();
-            DataTable dt_Supplier = new DataTable();
+            DataTable dt = new DataTable();
 
 
             Strsql = "Select * FROM vewCountry";
@@ -48,63 +45,181 @@ namespace Import_Freight_BOI.Controllers
             Strsql = "Select * FROM vewForwarder";
             ViewBag.Forwarder = ObjRun.GetDatatables(Strsql);
 
-            Strsql = "Select * FROM vewPurpose";
-            ViewBag.Purpose = ObjRun.GetDatatables(Strsql);
-
             Strsql = "Select * FROM vewSupplier";
             ViewBag.Supplier = ObjRun.GetDatatables(Strsql);
 
+            Strsql = "Select Value1 FROM Purpose where code= '002'";
+            dt = ObjRun.GetDatatables(Strsql);
+            ViewBag.Purpose = dt.Rows[0][0];
+
             Strsql = "Select * FROM vewTransportation";
             ViewBag.Transportation = ObjRun.GetDatatables(Strsql);
+            string Session = HttpContext.Session.GetString(SessionID);
+            ViewBag.SessionID = Session;
 
-            ViewBag.SessionID =  HttpContext.Session.GetString(SessionID);
-
-            return View();
+            if (Session == null)
+            {
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                return View();
+            }
+           
         }
         public IActionResult Report_Section()
         {
+            string Session = HttpContext.Session.GetString(SessionID);
 
-
-            string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-            string fileName = "authors.xlsx";
-            try
+            if (Session == null)
             {
-                using (var workbook = new XLWorkbook())
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+
+
+                string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                string fileName = "Report_Section.xlsx";
+                try
                 {
-                    IXLWorksheet worksheet =
-                    workbook.Worksheets.Add("Authors");
-                    worksheet.Cell(1, 1).Value = "Id";
-                    worksheet.Cell(1, 2).Value = "FirstName";
-                    worksheet.Cell(1, 3).Value = "LastName";
-
-                    workbook.Worksheets.Add("A");
-                    worksheet.Cell(1, 1).Value = "Id";
-                    worksheet.Cell(1, 2).Value = "FirstName";
-                    worksheet.Cell(1, 3).Value = "LastName";
-
-                    //required using System.IO;
-                    using (var stream = new MemoryStream())
+                    using (var workbook = new XLWorkbook())
                     {
-                        workbook.SaveAs(stream);
-                        var content = stream.ToArray();
-                        return File(content, contentType, fileName);
+                        IXLWorksheet worksheet =
+                        workbook.Worksheets.Add("Section");
+                        worksheet.Cell(1, 1).Value = "Export Frieght "+ '"' + "sort by Section"+'"' + "";
+                        worksheet.Cell(1, 2).Value = "TYPE";
+                        worksheet.Cell(1, 3).Value = "LastName";
+
+
+
+                        //required using System.IO;
+                        using (var stream = new MemoryStream())
+                        {
+                            workbook.SaveAs(stream);
+                            var content = stream.ToArray();
+                            return File(content, contentType, fileName);
+                        }
                     }
                 }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+
+
+                return View();
             }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-
-
-            return View();
         }
 
         public IActionResult Login()
         {
 
             return View();
+        }
+        public IActionResult Export_data()
+        {
+            string Session = HttpContext.Session.GetString(SessionID);
+
+            if (Session == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+
+                string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                string fileName = "Import freight.xlsx";
+                try
+                {
+                    using (var workbook = new XLWorkbook())
+                    {
+                        IXLWorksheet worksheet =
+                        workbook.Worksheets.Add("freight");
+                        worksheet.Cell(1, 1).Value = "Id";
+                        worksheet.Cell(1, 2).Value = "FirstName";
+                        worksheet.Cell(1, 3).Value = "LastName";
+
+                        workbook.Worksheets.Add("A");
+                        worksheet.Cell(1, 1).Value = "Id";
+                        worksheet.Cell(1, 2).Value = "FirstName";
+                        worksheet.Cell(1, 3).Value = "LastName";
+
+                        //required using System.IO;
+                        using (var stream = new MemoryStream())
+                        {
+                            workbook.SaveAs(stream);
+                            var content = stream.ToArray();
+                            return File(content, contentType, fileName);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+                return View();
+
+            }
+        }
+
+
+        public IActionResult Report_by_DN()
+        {
+
+            string Session = HttpContext.Session.GetString(SessionID);
+
+            if (Session == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+
+                string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                string fileName = "Report_DN.xlsx";
+                try
+                {
+                    using (var workbook = new XLWorkbook())
+                    {
+                        IXLWorksheet worksheet =
+                        workbook.Worksheets.Add("DN");
+                        worksheet.Cell(1, 1).Value = "ID";
+                        worksheet.Cell(1, 2).Value = "D/N";
+                        worksheet.Cell(1, 3).Value = "TITLE";
+                        worksheet.Cell(1, 4).Value = "SURSENSE";
+                        worksheet.Cell(1, 5).Value = "FREIGHT";
+                        worksheet.Cell(1, 6).Value = "SHIPPING";
+                        worksheet.Cell(1, 7).Value = "ORIGINAL";
+                        worksheet.Cell(1, 8).Value = "VAT1";
+                        worksheet.Cell(1, 9).Value = "BASE TAX3";
+                        worksheet.Cell(1, 10).Value = "BASE TAX1";
+                        worksheet.Cell(1, 11).Value = "TYPE";
+
+                        //workbook.Worksheets.Add("A");
+                        //worksheet.Cell(1, 1).Value = "Id";
+                        //worksheet.Cell(1, 2).Value = "FirstName";
+                        //worksheet.Cell(1, 3).Value = "LastName";
+
+                        //required using System.IO;
+                        using (var stream = new MemoryStream())
+                        {
+                            workbook.SaveAs(stream);
+                            var content = stream.ToArray();
+                            return File(content, contentType, fileName);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+                return View();
+
+            }
         }
 
 
@@ -143,6 +258,7 @@ namespace Import_Freight_BOI.Controllers
         {
             string status;
             var query = new mgrSQLConnect(_configuration);
+          
             string strQuery = "select * from vewOperator where OperatorID = '" + OPID.Trim() + "' and Password = '" + Password.Trim() + "'";
             var checkUser = query.GetDatatables(strQuery);
             if (checkUser.Rows.Count != 0)
@@ -157,6 +273,74 @@ namespace Import_Freight_BOI.Controllers
             }
 
             return Json(status);
+        }
+
+        public JsonResult Insert_Header(string DN , string YearMonth ,string Forwarder, string Invoice, string RentDay, string Vat, string Transport, string Delivery, string Supplier,
+                                        string Vm, string ETA ,string Storage, string Country)
+        {
+            string Session = HttpContext.Session.GetString(SessionID);
+            var Sqlquery = new mgrSQLConnect(_configuration);
+            DataTable dt = new DataTable();
+            string status;
+            string lastID;
+            try
+            {
+               
+                string Sqlstr = "Exec sprOperation_InvoiceImportHeader '" + DN + "','" + YearMonth + "','" + Forwarder + "','" + Invoice + "','" + RentDay +  "','" + Transport + "','" + Delivery + "','" + Supplier + "','" + Vm + "','" + ETA + "','" + Storage + "','" + Country + "','" + Vat + "','" + Session + "'";
+                var ExcuteProc = Sqlquery.ExcuteProc(Sqlstr);
+
+                 dt = Sqlquery.GetDatatables("select Max(DNID) from ImportFreightHead");
+                lastID = dt.Rows[0][0].ToString();
+
+                status = "True";
+
+            } catch (Exception ex){
+                throw ex;
+
+                status = "False";
+            }
+
+            var JsonResult = Json(new {status=  status , lastID = lastID });
+            return JsonResult;
+        }
+
+        //public JsonResult Insert_Freight_detail(string title , string terminal, string License, string Freight, string  Origin, string Shipping, string Vat1 , string Btax3 , string Btax1, string Vat2 , string Advance, string APamount)
+        //{
+
+        //    
+        //    return Json("");
+        //}
+
+
+        public JsonResult Insert_Freight_detail(string title, string terminal, string License, string Freight, string Origin, string Shipping, string Vat1, string Btax3, string Btax1, string Vat2, string Advance, string APamount)
+        {
+
+           
+            var Sqlquery = new mgrSQLConnect(_configuration);
+            DataTable dt = new DataTable();
+            string status;
+            string lastID;
+            try
+            {
+
+                //string Sqlstr = "Exec sprOperation_InvoiceImportHeader '" + DN + "','" + YearMonth + "','" + Forwarder + "','" + Invoice + "','" + RentDay + "','" + Transport + "','" + Delivery + "','" + Supplier + "','" + Vm + "','" + ETA + "','" + Storage + "','" + Country + "','" + Vat + "','" + Session + "'";
+                //var ExcuteProc = Sqlquery.ExcuteProc(Sqlstr);
+
+                //dt = Sqlquery.GetDatatables("select Max(DNID) from ImportFreightHead");
+                //lastID = dt.Rows[0][0].ToString();
+
+                //status = "True";
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+                status = "False";
+            }
+
+            var JsonResult = Json(new { status = "", lastID = "" });
+            return JsonResult;
         }
 
     }
